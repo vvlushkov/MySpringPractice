94f58b7ee4adf0fcbd7a2d0003b22f44f70f90f8
Victor Lushkov <viktor20010627@gmail.com>
2022-05-16 20:15:56 +0300

    /

--- src/main/java/com/lushkov/springpractice/config/WebSecurityConfig.java
+++ src/main/java/com/lushkov/springpractice/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.lushkov.springpractice.config;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
