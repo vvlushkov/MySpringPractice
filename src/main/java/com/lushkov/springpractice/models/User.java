483379df6699fa41241b9b8d0a5513163752dc30
Victor Lushkov <viktor20010627@gmail.com>
2022-06-11 20:30:28 +0300

    added two entity-classes

---
+++ src/main/java/com/lushkov/springpractice/models/User.java
@@ -0,0 +1,178 @@
+package com.lushkov.springpractice.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name="user_table")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(unique = true)
+    private String login;
+
+    @Column
+    private Date dob;
+
+    @Column
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    public User() {
+    }
+
+    public User(Long userId,
+                Role role,
+                String firstName,
+                String lastName,
+                String login,
+                Date dob,
+                String password,
+                String email) {
+        this.userId = userId;
+        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.login = login;
+        this.dob = dob;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User(Role role,
+                String firstName,
+                String lastName,
+                String login,
+                Date dob,
+                String password,
+                String email) {
+        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.login = login;
+        this.dob = dob;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!userId.equals(user.userId)) return false;
+        if (!login.equals(user.login)) return false;
+        if (!password.equals(user.password)) return false;
+        return email.equals(user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId.hashCode();
+        result = 31 * result + login.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + email.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", role=" + role +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", login='" + login + '\'' +
+                ", dob=" + dob +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
